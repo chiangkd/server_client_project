b14c6bd886bbb30efcec677f6c7ac6e18017764a
chiangkd <chiangkuandi@gmail.com>
2022-06-13 04:41:54 +0800

    Add commit

--- test_web/testweb.c
+++ test_web/testweb.c
@@ -531,6 +531,7 @@ void serve_static(int out_fd, int in_fd, http_request *req,
 
 	writen(out_fd, buf, strlen(buf));
 	off_t offset = req->offset; /* copy */
+	/* if file is too big, use while to load it */
 	while(offset < req->end) {
 		if(sendfile(out_fd, in_fd, &offset, req->end - req->offset) <= 0) {
 			break;
